141b04039fddd0c130ece7f67c5974644e894a4a
R-o-t-I <rulers.oftheinfinite@mail.ru>
2022-10-12 21:28:52 +0300

    fixed

--- src/structure.js
+++ src/structure.js
@@ -1,19 +1,5 @@
 const structure = [
   {
-    id: "profile",
-    hash: "profile",
-    panels: [
-      {
-        id: "admin",
-        hash: "/admin",
-      },
-      {
-        id: "base",
-        hash: "/base",
-      },
-    ],
-  },
-  {
     id: "home",
     hash: "home",
     panels: [
@@ -30,6 +16,20 @@ const structure = [
       {
         id: "base",
         hash: "/base",
+      },
+    ],
+  },
+  {
+    id: "profile",
+    hash: "profile",
+    panels: [
+      {
+        id: "base",
+        hash: "/base",
+      },
+      {
+        id: "admin",
+        hash: "/admin",
       },
     ],
   },
