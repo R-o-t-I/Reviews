d00bd2a6d67b4b7bb41915777fdafcc661b1828d
Evgeniy <50876285+evgrg@users.noreply.github.com>
2022-12-14 15:24:11 +0300

    Update base.js

--- src/js/panels/home/base.js
+++ src/js/panels/home/base.js
@@ -319,27 +319,6 @@ function HomePanel({ router }) {
             <Placeholder>Мечты загружаются...</Placeholder>
           </div>
         )}
-        <InfiniteScroll
-          dataLength={info.length} //This is important field to render the next data
-          next={() => getList()}
-          hasMore={true}
-          loader={<h4>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b>Yay! You have seen it all</b>
-            </p>
-          }
-          refreshFunction={true}
-          // below props only if you need pull down functionality
-          pullDownToRefresh
-          pullDownToRefreshThreshold={50}
-          pullDownToRefreshContent={
-            <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
-          }
-          releaseToRefreshContent={
-            <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
-          }
-        >
         {info.map((item, index) => (
           <div className={style.blockReviews}>
             <SimpleCell
@@ -451,7 +430,6 @@ function HomePanel({ router }) {
             </div>
           </div>
         ))}
-        </InfiniteScroll>
       </div>
       {snackbar}
     </>
